ded4861902115296c0a0cceeb70c3b1821342ed0
Maulana Khoirusyifa <91378553+MaulK@users.noreply.github.com>
2023-12-01 06:18:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector('header');
     const nav = document.querySelector('nav');
-    const mainContent = document.querySelector('.main-content');
-    const features = document.querySelector('.features');
-
-
+    const mainContent = document.querySelector('.main-content'); 
+    
     header.style.opacity = 0;
     nav.style.opacity = 0;
     mainContent.style.opacity = 0;
-    features.style.opacity = 0;
 
     setTimeout(() => {
         header.style.transition = 'opacity 1s ease-in-out';
         nav.style.transition = 'opacity 1s ease-in-out';
         mainContent.style.transition = 'opacity 1s ease-in-out';
-        features.style.transition = 'opacity 1s ease-in-out';
 
         header.style.opacity = 1;
         nav.style.opacity = 1;
         mainContent.style.opacity = 1;
-        features.style.opacity = 1;
     }, 500);
 });
 
